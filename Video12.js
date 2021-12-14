4e4e5577df492da3a589fe589cc51ff3e30ae1da
Areeb <areebkhan280@gmail.com>
2021-12-13 21:28:03 -0600

    Final Inputs

--- Video12.js
+++ Video12.js
@@ -1,5 +1,7 @@
 let video = document.getElementById("video");
-
+let model; 
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 const setupCamera = () => {
   navigator.mediaDevices
     .getUserMedia({ video: 
@@ -10,4 +12,31 @@ const setupCamera = () => {
     });
 };
 
+const detect = asnyc () => {
+  const prediction = await model.estimatefaces(video, false);
+  console.log(prediction);
+  ctx.drawImage(video, 0, 0, 600, 400);
+  prediction.forEach((pred) => {
+    ctx.begintax();
+    ctx.linewidth = "4";
+    ctx.strokeStyle = "red";
+    ctx.rect(
+      pred.topleft[0],
+      pred.topleft[1],
+      pred.bottomright[0] - pred.topleft[0],
+      pred.bottomright[1] - pred.topleft[1]
+    );
+  ctx.stroke();
+
+  ctx.fillstyle = "red";
+  pred.landmarks.forEach((landmark) => {
+    ctx.fillRect(landmark[0], landmark[1], 5, 5);
+  }
+  });
+};
+
 setupCamera();
+video.addEventListener("play", asnyc () => {
+  model = blazeface.load();
+  setInterval(detect, 100); 
+}); 
